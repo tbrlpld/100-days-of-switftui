60063cd33642aa02fc94c4e22f033a81ef4f80b9
Tibor Leupold <tibor@lpld.io>
2023-04-23 16:50:10 -0700

    Create check amount input and display

--- project-1/WeSplit/WeSplit/ContentView.swift
+++ project-1/WeSplit/WeSplit/ContentView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    
     var body: some View {
-        Text("Hello World!")
-            .padding()
+        Form {
+            Section {
+                TextField(
+                    "Amount",
+                    value: self.$checkAmount,
+                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                ).keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(
+                    self.checkAmount,
+                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+            }
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {

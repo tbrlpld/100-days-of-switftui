dac5d00e2b20407eaf4a365c50f661be73856b31
Tibor Leupold <tibor@lpld.io>
2023-04-23 10:07:58 -0700

    Day 17 commit (set up app)

---
+++ project-1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tibor Leupold on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

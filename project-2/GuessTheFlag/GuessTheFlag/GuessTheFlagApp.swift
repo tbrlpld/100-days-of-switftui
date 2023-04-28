0c5b1a018d7fc9ed6bfedf35fabd17049325ffa9
Tibor Leupold <tibor@lpld.io>
2023-04-27 18:51:14 -0700

    Set up project 2

---
+++ project-2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tibor Leupold on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

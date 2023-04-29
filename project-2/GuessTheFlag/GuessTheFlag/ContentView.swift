5bed23c04a04fe8d6bbe74ca0dbf3850a8e31b1e
Tibor Leupold <tibor@lpld.io>
2023-04-28 20:55:09 -0700

    Add score count

--- project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State var message = "Answer"
     @State var messageButtonText = "Continue"
     
+    @State var score = 0
+    
     init() {
         self.countries = Self.getCountryArray()
         self.correctCountry = Self.getCorrectAnswer()
@@ -22,27 +24,38 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color(red: 0.95, green: 0.95, blue: 0.95), Color(red: 0.8, green: 0.8, blue: 0.8)], startPoint: .top, endPoint: .bottom)
+            Color(red: 0.9, green: 0.9, blue: 0.9)
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Select the flag of")
-                        .font(.headline.weight(.heavy))
-                    Text(self.countries[self.correctCountry])
-                        .font(.largeTitle.weight(.bold))
-                }
-                
-                ForEach(0..<3) { number in
-                    Button {
-                        self.checkAnwer(answer: number)
-                    } label: {
-                        Image(self.countries[number].lowercased())
-                            .clipShape(Rectangle()).cornerRadius(5)
-                            .shadow(radius: 10, x: 2, y: 3)
+           
+            VStack {
+                Spacer()
+                VStack(spacing: 40) {
+                    VStack {
+                        Text("Select the flag of")
+                            .font(.headline.weight(.bold))
+                        Text(self.countries[self.correctCountry])
+                            .font(.largeTitle.weight(.bold))
+                    }
+                    
+                    VStack(spacing: 30) {
+                        ForEach(0..<3) { number in
+                            Button {
+                                self.checkAnwer(answer: number)
+                            } label: {
+                                Image(self.countries[number].lowercased())
+                                    .clipShape(Rectangle()).cornerRadius(5)
+                                    .shadow(radius: 10, x: 2, y: 3)
+                            }
+                        }
                     }
                 }
+                Spacer()
+                Text("Score: \( self.score )")
+                    .fontWeight(.semibold)
+                Spacer()
             }
-        }.alert(self.message, isPresented: self.$showResponse) {
+        }
+        .alert(self.message, isPresented: self.$showResponse) {
             Button(self.messageButtonText) {
                 self.askQuestion()
             }
@@ -59,9 +72,9 @@ struct ContentView: View {
     
     func checkAnwer(answer: Int) {
         if answer == self.correctCountry {
-            self.setResponseForCorrect()
+            self.handleCorrectAnswer()
         } else {
-            self.setResponseForWrong()
+            self.handleWrongAnswer()
         }
         self.showResponse = true
     }
@@ -71,12 +84,13 @@ struct ContentView: View {
         self.correctCountry = Self.getCorrectAnswer()
     }
     
-    func setResponseForCorrect() {
+    func handleCorrectAnswer() {
         self.message = "Correct"
         self.messageButtonText = "Continue"
+        self.score += 1
     }
     
-    func setResponseForWrong() {
+    func handleWrongAnswer() {
         self.message = "Wrong"
         self.messageButtonText = "New round"
     }

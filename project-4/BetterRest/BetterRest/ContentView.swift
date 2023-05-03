0b6b9216e72c6f2476f32e53356d1c37fd4a41ec
Tibor Leupold <tibor@lpld.io>
2023-05-03 15:51:51 -0700

    Add slep amount stepper

--- project-4/BetterRest/BetterRest/ContentView.swift
+++ project-4/BetterRest/BetterRest/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sleepAmount = 8.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Stepper(
+            "\(self.sleepAmount.formatted())",
+            value: self.$sleepAmount,
+            in: 4...12,
+            step: 0.25
+        )
     }
 }
 

28e2aa7eb80af625af90bef16e4d4a6d6100f64a
Tibor Leupold <tibor@lpld.io>
2023-05-02 17:12:06 -0700

    Set up challenge 2 project

---
+++ challenge-2/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Tibor Leupold on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

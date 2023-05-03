bf0a07a10d9cb0ede94b9e7c39e502bb12c7cfcc
Tibor Leupold <tibor@lpld.io>
2023-05-02 18:12:25 -0700

    Implement basic game logic

--- challenge-2/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ challenge-2/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -7,14 +7,26 @@
 
 import SwiftUI
 
+
+//  Each turn of the game the app will randomly pick either rock, paper, or scissors.
+//  Each turn the app will alternate between prompting the player to win or lose.
+//  The player must then tap the correct move to win or lose the game.
+//  If they are correct they score a point; otherwise they lose a point.
+//  The game ends after 10 questions, at which point their score is shown.
+
 let rock = "Rock"
 let paper = "Paper"
 let scissors = "Scissors"
 
 struct ContentView: View {
-    let hands = [rock, paper, scissors]
+    static let hands = [rock, paper, scissors]
     
-    @State var moveIsWin = Bool.random()
+    @State var challengerHand: String
+    @State var isMoveWin = Bool.random()
+    
+    init() {
+        self.challengerHand = Self.getRandomHand()
+    }
     
     var body: some View {
         VStack {
@@ -23,16 +35,17 @@ struct ContentView: View {
                 .font(.title).bold()
             Spacer()
             VStack(spacing: 20) {
-                Text(self.getRandomHand())
+                Text(self.challengerHand)
                     .font(.title).fontWeight(.bold)
                 Text("would")
                 Text(self.getMoveLabel())
                     .font(.title2).fontWeight(.bold)
                 Text("against?")
                 HStack(spacing: 30) {
-                    ForEach(self.hands, id: \.self) { hand in
+                    ForEach(Self.hands, id: \.self) { hand in
                         Button(hand) {
                             print("Selected \(hand)")
+                            handleHandPicked(picked: hand)
                         }
                         .bold()
                     }
@@ -45,18 +58,61 @@ struct ContentView: View {
         }
     }
     
-    func getRandomHand() -> String {
-        print("Getting random hand.")
-        let hand = self.hands.randomElement()!
-        print("-> \(hand)")
+    static func getRandomHand() -> String {
+        let hand = Self.hands.randomElement()!
+        print("Getting random hand: \(hand)")
         return hand
     }
     
+    func askQuestion() {
+        print()
+        self.isMoveWin.toggle()
+        self.challengerHand = Self.getRandomHand()
+    }
+    
     func getMoveLabel() -> String {
-        if self.moveIsWin {
+        if self.isMoveWin {
             return "win"
         }
         return "loose"
+    }
+    
+    func handleHandPicked(picked: String) {
+        let result: Bool
+        if self.isMoveWin {
+            result = self.wouldWinAgainst(challenger: self.challengerHand, incumbent: picked)
+        } else {
+            result = self.wouldLooseAgainst(challenger: self.challengerHand, incumbent: picked)
+        }
+        self.askQuestion()
+    }
+    
+    func wouldWinAgainst(challenger: String, incumbent: String) -> Bool {
+        print("\(challenger) would WIN against \(incumbent):")
+        if (
+            (challenger == rock && incumbent == scissors)
+            || (challenger == scissors && incumbent == paper)
+            || (challenger == paper && incumbent == rock)
+        ) {
+            print("TRUE")
+            return true
+        }
+        print("FALSE")
+        return false
+    }
+    
+    func wouldLooseAgainst(challenger: String, incumbent: String) -> Bool {
+        print("\(challenger) would LOOSE against \(incumbent):")
+        if (
+            (challenger == scissors && incumbent == rock)
+            || (challenger == paper && incumbent == scissors)
+            || (challenger == rock && incumbent == paper)
+        ) {
+            print("TRUE")
+            return true
+        }
+        print("FALSE")
+        return false
     }
 }
 

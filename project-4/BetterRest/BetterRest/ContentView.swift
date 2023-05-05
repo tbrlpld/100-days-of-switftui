a0afd3c4ea7d4e70ae479f72ac62a9765b778729
Tibor Leupold <tibor@lpld.io>
2023-05-04 19:28:30 -0700

    Set up basic app structure

--- project-4/BetterRest/BetterRest/ContentView.swift
+++ project-4/BetterRest/BetterRest/ContentView.swift
@@ -8,45 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUpTime = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack(spacing: 20) {
-            Stepper(
-                "\(self.sleepAmount.formatted())",
-                value: self.$sleepAmount,
-                in: 4...12,
-                step: 0.25
-            )
-            DatePicker(
-                "Wake up time?",
-                selection: self.$wakeUpTime,
-                in: Date.now...
-            )
-            Text(self.wakeUpTime, format: .dateTime.hour().minute().second())
-            Text(self.wakeUpTime.formatted(date: .long, time: .shortened))
-            Button("Test") {
-                self.example()
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("When you want to wake up?")
+                    .font(.headline)
+                DatePicker(
+                    "",
+                    selection: self.$wakeUpTime,
+                    displayedComponents: .hourAndMinute
+                )
+                    .labelsHidden()
+                
+                Text("How much sleep do you like?")
+                    .font(.headline)
+                Stepper(
+                    "\(self.sleepAmount.formatted()) hours",
+                    value: self.$sleepAmount,
+                    in: 4...12,
+                    step: 0.25
+                )
+                
+                Text("How much coffee do you drink?")
+                    .font(.headline)
+                Stepper(
+                    self.coffeeAmount == 1 ? "1 cup" : "\(self.coffeeAmount) cups",
+                    value: self.$coffeeAmount,
+                    in: 1...20
+                )
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Continue") {
+                    self.next()
+                }
             }
         }
-        .padding()
     }
     
-    func example() {
-        var components = DateComponents()
-        components.hour = 8
-        components.minute = 31
-        
-        let dateWithSetHour = Calendar.current.date(bySetting: .hour, value: 8, of: Date.now) ?? Date.now
-        let date = Calendar.current.date(bySetting: .minute, value: 31, of: dateWithSetHour) ?? Date.now
-        print(type(of: date))
-        print(date.formatted(date: .long, time: .complete))
-        
-        let nowComponents = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = nowComponents.hour ?? 0
-        let minute = nowComponents.minute ?? 0
-        print("\(hour):\(minute)")
+    func next() {
+        print("Button pressed.")
     }
 }
 

929b2fe6ab522fa0ef3549676bfa194ccea838c9
Tibor Leupold <tibor@lpld.io>
2023-05-04 20:55:29 -0700

    Implement app logic

--- project-4/BetterRest/BetterRest/ContentView.swift
+++ project-4/BetterRest/BetterRest/ContentView.swift
@@ -4,13 +4,22 @@
 //
 //  Created by Tibor Leupold on 5/3/23.
 //
+// This app calculated the recommended bedtime, based on when you want to wake up,
+// how much sleep you typically like to get (you already need to know this), and
+// how much coffee you drink. Drinking coffee during the day, usually means you
+// need more time in bed to reach the desired amount of restful sleep.
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUpTime = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = "Recommended bed time"
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationView {
@@ -44,15 +53,58 @@ struct ContentView: View {
             .padding()
             .navigationTitle("BetterRest")
             .toolbar {
-                Button("Continue") {
-                    self.next()
+                Button("Caclulate") {
+                    self.calculateBedTime()
                 }
+            }
+            .alert(self.alertTitle, isPresented: self.$showAlert) {
+                Button("Ok") {}
+            } message: {
+                Text(self.alertMessage)
             }
         }
     }
     
-    func next() {
-        print("Button pressed.")
+    func calculateBedTime() {
+        print("Calculating bed time...")
+        let wakeUpTimeComponents = Calendar.current.dateComponents([.hour, .minute], from: self.wakeUpTime)
+        let wakeUpTimeSeconds = (
+            self.hoursInSeconds(wakeUpTimeComponents.hour ?? 0)
+            + self.minutesInSeconds(wakeUpTimeComponents.minute ?? 0)
+        )
+        print(wakeUpTimeSeconds)
+        
+        let config = MLModelConfiguration()
+        
+        do {
+            let model = try BedTimeCalculator(configuration: config)
+            let prediction = try model.prediction(
+                wake: Double(wakeUpTimeSeconds),
+                estimatedSleep: Double(self.sleepAmount),
+                coffee: Double(self.coffeeAmount)
+            )
+            let recommendedSleepAmountSeconds = prediction.actualSleep
+            let recommendedBedTime = self.wakeUpTime - recommendedSleepAmountSeconds
+            self.alertMessage = String(recommendedBedTime.formatted(date: .omitted, time: .shortened))
+        } catch {
+            self.alertTitle = "Error"
+            self.alertMessage = "Sorry, something went wrong."
+        }
+        
+        self.showAlert = true
+    }
+    
+    func hoursInMinutes(_ hours: Int) -> Int {
+        return hours * 60
+    }
+    
+    func minutesInSeconds(_ minutes: Int) -> Int {
+        return minutes * 60
+    }
+    
+    func hoursInSeconds(_ hours: Int) -> Int {
+        let minutes = self.hoursInMinutes(hours)
+        return self.minutesInSeconds(minutes)
     }
 }
 

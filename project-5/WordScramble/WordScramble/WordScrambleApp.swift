47af2dbec801d6b5de95eeec8a04fd836be78e7e
Tibor Leupold <tibor@lpld.io>
2023-05-06 08:32:42 -0700

    Set up word scramble project

---
+++ project-5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tibor Leupold on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

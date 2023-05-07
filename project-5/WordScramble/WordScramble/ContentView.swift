2f3f331252afbf1021b083226a697f30b7429523
Tibor Leupold <tibor@lpld.io>
2023-05-07 10:03:16 -0700

    Add validation logic

--- project-5/WordScramble/WordScramble/ContentView.swift
+++ project-5/WordScramble/WordScramble/ContentView.swift
@@ -12,12 +12,17 @@ struct ContentView: View {
     @State private var newWord = ""
     @State private var enteredWords = [String]()
     
+    @State private var wordErrorTitle = ""
+    @State private var wordErrorMessage = ""
+    @State private var showWordError = false
+    
     var body: some View {
         NavigationView {
             List{
                 Section {
                     TextField("Enter new word", text: self.$newWord)
                         .autocapitalization(.none)
+                        .autocorrectionDisabled()
                 } header: {
                     Text("Make new words from the letters in '\(self.rootWord)'")
                 }
@@ -37,19 +42,11 @@ struct ContentView: View {
         }
         .onAppear(perform: self.startGame)
         .onSubmit(self.addNewWord)
-    }
-    
-    func addNewWord() {
-        print("New word submitted")
-        let value = self.newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        // Check that the value has some characters in it.
-        guard value.count > 0 else { return }
-        
-        withAnimation {
-            self.enteredWords.insert(value, at: 0)
+        .alert(self.wordErrorTitle, isPresented: self.$showWordError) {
+            Button("Ok", role: .cancel) {}
+        } message: {
+            Text(self.wordErrorMessage)
         }
-        
-        self.newWord = ""
     }
     
     func startGame() {
@@ -65,6 +62,72 @@ struct ContentView: View {
         let startWords = startWordsContent.components(separatedBy: "\n")
         self.rootWord = startWords.randomElement() ?? "silkworm"
         print(self.rootWord)
+    }
+    
+    func addNewWord() {
+        print("New word submitted")
+        let value = self.newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        // Check that the value has some characters in it.
+        guard value.count > 0 else { return }
+        
+        guard self.isOriginal(word: value) else {
+            self.wordError(title: "Already used", message: "Be more original.")
+            return
+        }
+        
+        guard self.isPossible(word: value) else {
+            self.wordError(title: "Not possible", message: "You can not build '\(value)' from '\(self.rootWord)'")
+            return
+        }
+        
+        guard self.isRealWord(word: value) else {
+            self.wordError(title: "Not a word", message: "You have to use real words, right?!")
+            return
+        }
+         
+        withAnimation {
+            self.enteredWords.insert(value, at: 0)
+        }
+        
+        self.newWord = ""
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !self.enteredWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var referenceWord = self.rootWord
+        for letter in word {
+            if let pos = referenceWord.firstIndex(of: letter) {
+                // If the letter was found, remove it from the reference word.
+                referenceWord.remove(at: pos)
+            } else {
+                // If the letter was not found in the reference, the word is invalid.
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isRealWord(word: String) -> Bool {
+        let range = NSRange(location: 0, length: word.count)
+        let checker = UITextChecker()
+        let misspelledWord = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en"
+        )
+        let isErrorFree = misspelledWord.location == NSNotFound
+        return isErrorFree
+    }
+    
+    func wordError(title: String, message: String) {
+        self.wordErrorTitle = title
+        self.wordErrorMessage = message
+        self.showWordError = true
     }
 }
 

6119d6f889ee493833a5e6e56f8ceb0f415c3502
Tibor Leupold <tibor@lpld.io>
2023-05-07 08:36:05 -0700

    Add ability to enter words into a list

--- project-5/WordScramble/WordScramble/ContentView.swift
+++ project-5/WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var enteredWords = [String]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List{
+                Section {
+                    TextField("Enter new word", text: self.$newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(self.enteredWords, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
-        .padding()
+        .onSubmit{self.addNewWord()}
+    }
+    
+    func addNewWord() {
+        let value = self.newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        // Check that the value has some characters in it.
+        guard value.count > 0 else { return }
+        
+        withAnimation {
+            self.enteredWords.insert(value, at: 0)
+        }
+        
+        self.newWord = ""
     }
 }
 

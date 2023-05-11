795cb269f932305e9d04853658e08759fd25c62a
Tibor Leupold <tibor@lpld.io>
2023-05-10 20:49:41 -0700

    Create a custom pivot animation

--- project-6/Animations/Animations/ContentView.swift
+++ project-6/Animations/Animations/ContentView.swift
@@ -7,21 +7,44 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let corner: UnitPoint
+    let angle: Double
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(self.angle), anchor: self.corner)
+            .clipped()
+    }
+    
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(corner: .topLeading, angle: 90),
+            identity: CornerRotateModifier(corner: .topLeading, angle: 0)
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingCard = false
     
     var body: some View {
-        Button("Tap me") {
-            withAnimation{
-                self.isShowingCard.toggle()
+        VStack {
+            Button("Tap me") {
+                withAnimation{
+                    self.isShowingCard.toggle()
+                }
             }
-        }
-        
-        if self.isShowingCard {
-            LinearGradient(colors: [.yellow, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
-                .frame(width: 300, height: 200)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .transition(.scale)
+            
+            if self.isShowingCard {
+                LinearGradient(colors: [.yellow, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .frame(width: 300, height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .transition(.pivot)
+            }
         }
     }
 }

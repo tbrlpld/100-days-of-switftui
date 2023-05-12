2c631cf6614a9a63d897c9406b4e27629e66b2b6
Tibor Leupold <tibor@lpld.io>
2023-05-12 15:52:13 -0700

    Add app for challenge 3

---
+++ challenge-3/MultiplicationTables/MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Tibor Leupold on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

fefaeb8940e1bddfe6f7cb1026c0ed0ee22fb321
Tibor Leupold <tibor@lpld.io>
2023-05-14 14:40:16 -0700

    Add project 7

---
+++ project-7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tibor Leupold on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

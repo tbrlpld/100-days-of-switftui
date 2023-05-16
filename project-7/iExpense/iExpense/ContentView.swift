1c21b350e68a1cdcca5171d8968dd23d619a04aa
Tibor Leupold <tibor@lpld.io>
2023-05-16 15:29:10 -0700

    Delete from items list based on id

--- project-7/iExpense/iExpense/ContentView.swift
+++ project-7/iExpense/iExpense/ContentView.swift
@@ -48,8 +48,19 @@ struct ContentView: View {
     
     func removeItem (at offset: IndexSet) {
         print("Deleting item")
-        self.expenses.items.remove(atOffsets: offset)
-    }
+        // Get index
+        guard let index = offset.first else { return }
+        // Get item at index
+        let item = self.expenses.items[index]
+        // Delete the list item that matches the selected item.
+        // Currently this makes no sense because the array from which we retrieve the reference
+        // is the same as the one we are manupilating.
+        // But, this unlocks using different lists.
+        // E.g. the item may be selected on a sublist, which will have different indecies than the list we delete from.
+        self.expenses.items.removeAll { expenseItem in
+            expenseItem.id == item.id
+        }
+      }
     
     func amountColor(amount: Double) -> Color {
         if amount < 10 {

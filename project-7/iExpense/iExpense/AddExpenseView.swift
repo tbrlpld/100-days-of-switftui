2725c58270109b0bae51b39563ff27e826349ea7
Tibor Leupold <tibor@lpld.io>
2023-05-16 15:04:12 -0700

    Use users preferred currency

--- project-7/iExpense/iExpense/AddExpenseView.swift
+++ project-7/iExpense/iExpense/AddExpenseView.swift
@@ -30,7 +30,7 @@ struct AddExpenseView: View {
                 }
                 .pickerStyle(.navigationLink)
                 
-                TextField("Amount", value: self.$amount, format: .currency(code: "USD"))
+                TextField("Amount", value: self.$amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add expense")

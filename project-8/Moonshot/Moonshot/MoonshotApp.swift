519339b0a0131519d635c3fccc21ea147921537b
Tibor Leupold <tibor@lpld.io>
2023-05-17 17:59:34 -0700

    Add project 8

---
+++ project-8/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tibor Leupold on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

84d19dd77625dbacc2b58830f9fefe522a7f09b4
Tibor Leupold <tibor@lpld.io>
2023-05-17 19:41:41 -0700

    Reset app

--- project-8/Moonshot/Moonshot/ContentView.swift
+++ project-8/Moonshot/Moonshot/ContentView.swift
@@ -8,15 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
-    
     var body: some View {
-        LazyVGrid(columns: self.layout) {
-            ForEach(0..<100) {
-                Text("Item \($0)")
-            }
+        VStack{
+            Text("Hello World")
         }
     }
 }

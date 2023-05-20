53a40ff10d733c50252b63da8030f1945b928c28
Tibor Leupold <tibor@lpld.io>
2023-05-20 10:45:37 -0700

    Add ability to toggle content view between grid and list

--- project-8/Moonshot/Moonshot/ContentView.swift
+++ project-8/Moonshot/Moonshot/ContentView.swift
@@ -15,36 +15,87 @@ struct ContentView: View {
         GridItem(.adaptive(minimum: 150))
     ]
     
+    @State private var isShowingAsList = true
+    
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: self.layout){
-                    ForEach(self.missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: self.astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.imageName)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80)
-                                Text(mission.displayName)
-                                    .font(.headline)
-                                Text(mission.displayLaunchDate)
-                                    .font(.subheadline)
-                                    .opacity(0.8)
-                            }
-                            .foregroundColor(.primary)
-                            .padding(20)
-                            .frame(maxWidth: .infinity)
-                        }
-                    }
+                if self.isShowingAsList {
+                    self.listView
+                } else {
+                    self.gridView
                 }
             }
             .preferredColorScheme(.dark)
             .navigationTitle("Moonshot")
+            .toolbar {
+                Button(self.listViewToggleButtonText) {
+                    self.isShowingAsList.toggle()
+                }
+            }
             .background(.darkBackground)
         }
+    }
+    
+    var gridView: some View {
+        LazyVGrid(columns: self.layout){
+            ForEach(self.missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: self.astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.displayLaunchDate)
+                            .font(.subheadline)
+                            .opacity(0.8)
+                    }
+                    .foregroundColor(.primary)
+                    .padding(20)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+        }
+    }
+    
+    var listView: some View {
+        VStack {
+            ForEach(self.missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: self.astronauts)
+                } label: {
+                    HStack(spacing: 20) {
+                        Image(mission.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                        
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                            Text(mission.displayLaunchDate)
+                                .font(.subheadline)
+                                .opacity(0.8)
+                        }
+                        
+                        Spacer()
+                        Image(systemName: "chevron.forward")
+                            .foregroundColor(.primary)
+                    }
+                    .foregroundColor(.primary)
+                    .padding(20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+        }
+    }
+    
+    var listViewToggleButtonText: String {
+        self.isShowingAsList ? "Grid" : "List"
     }
 }
 

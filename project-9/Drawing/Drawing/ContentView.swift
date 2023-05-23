85dabb1c1738645bc5c7d4a59df4d633d0a66701
Tibor Leupold <tibor@lpld.io>
2023-05-22 18:34:03 -0700

    Remove clash between prop and method

--- project-9/Drawing/Drawing/ContentView.swift
+++ project-9/Drawing/Drawing/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct Triangle: InsettableShape {
-    var inset = 0.0
+    var insetAmount = 0.0
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY + self.inset))
-        path.addLine(to: CGPoint(x: rect.minX + self.inset, y: rect.maxY - self.inset))
-        path.addLine(to: CGPoint(x: rect.maxX - self.inset, y: rect.maxY - self.inset))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY + self.inset))
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY + self.insetAmount))
+        path.addLine(to: CGPoint(x: rect.minX + self.insetAmount, y: rect.maxY - self.insetAmount))
+        path.addLine(to: CGPoint(x: rect.maxX - self.insetAmount, y: rect.maxY - self.insetAmount))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY + self.insetAmount))
         path.closeSubpath()
         
         return path
@@ -24,7 +24,7 @@ struct Triangle: InsettableShape {
     
     func inset(by amount: CGFloat) -> some InsettableShape {
         var triangle = self
-        triangle.inset = amount
+        triangle.insetAmount = amount
         return triangle
     }
 }
@@ -33,6 +33,7 @@ struct Triangle: InsettableShape {
 struct ContentView: View {
     var body: some View {
         Triangle()
+//            .stroke(.red, style: StrokeStyle(lineWidth: 40, lineCap: .round, lineJoin: .round))
             .strokeBorder(.red, style: StrokeStyle(lineWidth: 40, lineCap: .round, lineJoin: .round))
             .frame(height: 300)
     }

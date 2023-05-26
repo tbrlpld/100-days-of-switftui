62cee11536cde2c8398128267b2ea349368e66ef
Tibor Leupold <tibor@lpld.io>
2023-05-26 15:44:57 -0700

    Add challenge 4 projeject

---
+++ challenge-4/Habits/Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Tibor Leupold on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

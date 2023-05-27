c51707b40b66fdabe6e1e9e6e5384582e8231570
Tibor Leupold <tibor@lpld.io>
2023-05-26 17:41:18 -0700

    Store data in user defaults

--- challenge-4/Habits/Habits/Habit.swift
+++ challenge-4/Habits/Habits/Habit.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 
-struct Habit: Identifiable {
-    let id = UUID()
+struct Habit: Identifiable, Codable {
+    let id: UUID
     let name: String
     let description: String
+    
+    init(name: String, description: String) {
+        self.id = UUID()
+        self.name = name
+        self.description = description
+    }
 }
--- challenge-4/Habits/Habits/HabitsData.swift
+++ challenge-4/Habits/Habits/HabitsData.swift
@@ -7,8 +7,43 @@
 
 import Foundation
 
+
+let habitsDataKey = "habits"
+
+
 class HabitsData: ObservableObject {
-    @Published var habits: [Habit]
+    @Published var habits: [Habit] {
+        didSet {
+            print("Habits were updated, saving to user data.")
+            let encoder = JSONEncoder()
+            guard let encodedData = try? encoder.encode(self.habits) else {
+                print("Could not encode data")
+                return
+            }
+            print(encodedData)
+            UserDefaults.standard.set(encodedData, forKey: habitsDataKey)
+            print("Done storing data.")
+        }
+    }
+    
+    init() {
+        let defaultHabits = [Habit]()
+        
+        guard let encodedData = UserDefaults.standard.data(forKey: habitsDataKey) else {
+            print("Could not load data from user defaults")
+            self.habits = defaultHabits
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        guard let data = try? decoder.decode([Habit].self, from: encodedData) else {
+            print("Could not decode data from user defaults")
+            self.habits = defaultHabits
+            return
+        }
+        
+        self.habits = data
+    }
     
     init(habits: [Habit] = [Habit]()) {
         self.habits = habits

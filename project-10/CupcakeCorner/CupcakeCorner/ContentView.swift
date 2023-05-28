e062df48907e96f058f70fa0e08cb41d960465bf
Tibor Leupold <tibor@lpld.io>
2023-05-28 08:11:52 -0700

    Create codable class with published property

--- project-10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ project-10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Jane Doe"
+    
+    required init(from decoder: Decoder) throws {
+        // Get the decoded data where we expect the data to have all the keys from our "CodingKeys" enum.
+        let container = try decoder.container(keyedBy: Self.CodingKeys.self)
+        // Set the object values from the decoded data.
+        self.name = try container.decode(String.self, forKey: Self.CodingKeys.name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        // Create a container for the data we want to encode from the encoder that needs to be able to hold all the keys defined in our coding keys enum.
+        var container = try encoder.container(keyedBy: Self.CodingKeys.self)
+        // Encode the individual values.
+        try container.encode(self.name, forKey: Self.CodingKeys.name)
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {

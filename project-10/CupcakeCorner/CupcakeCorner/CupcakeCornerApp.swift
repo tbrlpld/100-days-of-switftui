c7586377582daafedbe09a0a7b44636ba981af73
Tibor Leupold <tibor@lpld.io>
2023-05-28 07:42:36 -0700

    Add project 10

---
+++ project-10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Tibor Leupold on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

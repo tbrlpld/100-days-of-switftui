80b4c1e3171696bdb19baf6cb01a901a64ae77e0
Tibor Leupold <tibor@lpld.io>
2023-05-30 18:02:17 -0700

    Add network call to post order details

--- project-10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ project-10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var orderConfirmationMessage = ""
+    @State private var isShowingConfirmationMessage = false
+    
     var body: some View {
         VStack {
             GeometryReader { geo in
@@ -29,14 +32,50 @@ struct CheckoutView: View {
                 .font(.headline)
                 .padding(.bottom, 20)
             
-            Button("Place order") {}
+            Button("Place order") {
+                Task {
+                    await self.placeOrder()
+                }
+            }
                 .buttonStyle(.borderedProminent)
             
             Spacer()
         }
         .padding(.horizontal)
         .navigationTitle("Checkout")
+        .alert("Success", isPresented: self.$isShowingConfirmationMessage) {
+            Button("Ok") {}
+        } message: {
+            Text(self.orderConfirmationMessage)
+        }
     }
+    
+    func placeOrder() async {
+        let encoder = JSONEncoder()
+        guard let encodedOrder = try? encoder.encode(self.order) else {
+            print("Encoding order failed.")
+            return
+        }
+        
+        guard let url = URL(string: "https://reqres.in/api/cupcakes") else {
+            fatalError("Could generate not URL for order placement.")
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (responseData, _) = try await URLSession.shared.upload(for: request, from: encodedOrder)
+            let decoder = JSONDecoder()
+            let decodedOrder = try decoder.decode(Order.self, from: responseData)
+            self.orderConfirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes was successfully placed."
+            self.isShowingConfirmationMessage = true
+        } catch {
+            print("Checkout failed.")
+        }
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {

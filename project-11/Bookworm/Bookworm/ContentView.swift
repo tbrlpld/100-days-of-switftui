d97c29499882f5e9a12b994dfd9bd59d16c8050a
Tibor Leupold <tibor@lpld.io>
2023-06-03 08:40:13 -0700

    Add basic content view

--- project-11/Bookworm/Bookworm/ContentView.swift
+++ project-11/Bookworm/Bookworm/ContentView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var isShowingAddBookView = false
+    
     var body: some View {
-        VStack {
-            Text("Hello World")
+        NavigationStack {
+            Text("\(books.count) Books read")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            self.isShowingAddBookView.toggle()
+                        } label: {
+                            Label("Add book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: self.$isShowingAddBookView) {
+                    Text("Hello Sheet")
+                }
         }
     }
 }

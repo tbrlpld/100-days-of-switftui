2afc4d8b3912ccd5a2d27273b8df4276c6474a42
Tibor Leupold <tibor@lpld.io>
2023-06-02 20:27:47 -0700

    Reset content view and data model

--- project-11/Bookworm/Bookworm/ContentView.swift
+++ project-11/Bookworm/Bookworm/ContentView.swift
@@ -9,34 +9,9 @@ import SwiftUI
 
 
 struct ContentView: View {
-    // Load data from the data store. The empty sort descriptor means items are randomly sorted.
-    // The Student class is automatically generated from the data model entity.
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-    
-    // Write data to the with the managed object that we get from the environment.
-    @Environment(\.managedObjectContext) var moc
-    
     var body: some View {
         VStack {
-            List(self.students) { student in
-                Text(student.name ?? "n/a")
-            }
-            Button("Add student") {
-                let firstNames = ["Harry", "Ron", "Hermoine", "Luna", "Ginny"]
-                let lastNames = ["Potter", "Weasley", "Granger", "Lovegood"]
-                
-                // Using ! here because we know the arrays are not empty we just defined them above
-                let firstName = firstNames.randomElement()!
-                let lastName = lastNames.randomElement()!
-                
-                // Pass the managed context object to which the entity should be saved.
-                let student = Student(context: self.moc)
-                student.id = UUID()
-                student.name = "\(firstName) \(lastName)"
-                
-                // Commit the new changes that are written to this managed object context.
-                try? moc.save()
-            }
+            Text("Hello World")
         }
     }
 }

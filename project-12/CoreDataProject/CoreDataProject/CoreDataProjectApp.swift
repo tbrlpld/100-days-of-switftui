203b7ae489c0af1e49b3ce15d8f645a217af58fc
Tibor Leupold <tibor@lpld.io>
2023-06-07 18:59:33 -0700

    Start project 12

---
+++ project-12/CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Tibor Leupold on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
